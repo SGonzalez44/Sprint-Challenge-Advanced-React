33954ac386baff958e993d6c7a6047c48c8253c5
Salvador <salgonzolanda@gmail.com>
2019-09-06 16:15:07 -0700

    mvp

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,13 +1,15 @@
 - [ ] Why would you use class component over function components (removing hooks from the question)?
-Because class components give you a lot of features to be able to make interactive user interfaces
 
-- [ ] Name three lifecycle methods and their purposes.
+Because class components give you a lot of features to be able to make interactive user interfaces including state but not limited too.
+
+- [x] Name three lifecycle methods and their purposes.
 1. Mounting 2. Unmounting 3. Updating
 
-- [ ] What is the purpose of a custom hook?
+- [x] What is the purpose of a custom hook?
 
-Be able to apply data and make dynamic user interface
+1. We can decide what the arguments and the returns are.
+2. Be able to apply custom data and make dynamic user interface
 
-- [ ] Why is it important to test our apps?
+- [x] Why is it important to test our apps?
 
-So that out applications dont crash and burn
+1. So that our applications dont crash and burn! Just kidding.. Testing ensures bugs are hashed out before production.
--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,55 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import axios from "axios";
-import PlayerList from "./components/PlayerList";
+import React from 'react';
+import './App.css';
+import DarkMode from "./hooks/DarkMode";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      players: {}
-    };
-  }
+    constructor() {
+        super();
+        this.state = {
+            players: []
+        };
+    }
 
-  componentDidMount = () =>
-    axios
-      .get("http://localhost:5000/api/players")
-      .then(res => this.setState({ players: res.data }));
+    componentDidMount() {
+        fetch('http://localhost:5000/api/players')
+            .then (res => res.json())
+            .then (data => this.setState({players:data}));
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <PlayerList players={this.state.players} />
-      </div>
-    );
-  }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log(this.state);
+    }
+
+    render(){
+        return(
+            <div className = "App">
+                <SoccerPlayer players = {this.state.players} />
+            </div>
+        );
+    }
 }
+
+    function SoccerPlayer(props) {
+    const [darkMode, setDarkMode] = DarkMode(false);
+    const toggleDark = e => {
+        e.preventDefault();
+        setDarkMode(!darkMode);
+    };
+    return(
+        <div>
+            <button onClick = {toggleDark}>Dark Mode</button>
+            <div className = "innerCard">
+            {props.players.map(player => (
+                <div className = "playerCard" key = {player.id}>
+                    <h1>{player.name}</h1>
+                    <p>{(player.id)+1}</p>
+                    <h2>{player.country}</h2>
+                    <p>{player.searches}</p>
+                </div>
+            ))}
+            </div>
+        </div>
+    )
+    }
+
 export default App;
